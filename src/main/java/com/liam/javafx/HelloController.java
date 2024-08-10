d6e1efe17e4381882376da6e1c424d8229bb0fba
liam <huzi_li57@163.com>
2024-08-10 11:39:02 +0800

    按钮点击隐藏逻辑

--- src/main/java/com/liam/javafx/HelloController.java
+++ src/main/java/com/liam/javafx/HelloController.java
@@ -8,7 +8,28 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Label newLabelText;
+
+    private boolean isHide;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        isHide = !isHide;
+        if (isHide) {
+            setNotHide();
+        } else {
+            setHide();
+        }
+    }
+
+    protected void setHide() {
+        newLabelText.setDisable(true);
+        newLabelText.setText(null);
+    }
+
+    protected void setNotHide() {
+        newLabelText.setDisable(false);
+        newLabelText.setText("this is new Label text");
     }
 }
